e0adb112af0c88c21d7b3dcbb873ed3f4112d03e
James Robins <jamesrobins101@hotmail.com>
2018-10-03 14:05:16 +0100

    starting router on app.js and user register page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Browser, Route, Switch } from 'react-router-dom';
 
+import Register from './components/Register';
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <Browser>
+
+        <div>
+          <h1>Art Mapper</h1>
+          <main className="section">
+            <div className="container">
+              <Switch>
+                <Route path="/register" component={Register} />
+              </Switch>
+            </div>
+          </main>
+        </div>
+      </Browser>
+
     );
   }
 }
---
+++ src/components/Register.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import axios from 'axios';
+
+// import Auth from '../lib/Auth';
+
+class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = { credentials: null };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const credentials = { ...this.state.credentials, [e.target.name]: e.target.value };
+    this.setState({ credentials, error: '' });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post('/api/login', this.state.credentials)
+      .then( res => {
+        // Auth.setToken(res.data.token);
+
+        this.props.history.push('/');
+      })
+      .catch(() => this.setState({error: 'Invalid credentials'}));
+  }
+
+  render() {
+    return (
+
+      <form onSubmit={this.handleSubmit}>
+        <p>Register page</p>
+        <div className="field">
+          <label className="label">User Name</label>
+          <div className="control">
+            <input className={`input ${this.state.error ? 'is-danger' : ''} `} name="username" placeholder="User Name" onChange={this.handleChange} />
+          </div>
+          <label className="label">Email</label>
+          <div className="control">
+            <input className={`input ${this.state.error ? 'is-danger' : ''} `} name="email" placeholder="Email" onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className={`input ${this.state.error ? 'is-danger' : ''} `} name="password" type="password" placeholder="Password" onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Password Confirmation</label>
+          <div className="control">
+            <input className={`input ${this.state.error ? 'is-danger' : ''} `} name="passwordConfirm" type="password" placeholder="Password" onChange={this.handleChange} />
+          </div>
+        </div>
+        {this.state.error && <small className="help is-danger">{this.state.error}</small>}
+
+        <button className="button is-primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default Register;
