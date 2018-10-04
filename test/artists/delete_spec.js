b119fc21dd3985e253d3144d5dd0a8951b0f8815
James Robins <jamesrobins101@hotmail.com>
2018-10-04 16:01:35 +0100

    created test spec for artist create

---
+++ test/artists/delete_spec.js
@@ -0,0 +1,94 @@
+/* global api, describe, it, expect, beforeEach */
+const jwt = require('jsonwebtoken');
+const { secret } = require('../../config/environment');
+const Artist = require('../../models/artist');
+const User = require('../../models/user');
+
+// let artistId = null;
+
+const artistData = {
+  name: 'test artist',
+  image: 'https://test.image',
+  dateBorn: 'January 1, 1881',
+  dateDeath: 'January 1, 1981',
+  info: 'test info',
+  wikiLink: 'https://wiki.link'
+};
+
+const userData = { username: 'test', email: 'test@test.com', password: 'test', passwordConfirmation: 'test' };
+let token;
+
+describe('DELETE /artists', () => {
+  beforeEach(done => {
+    Promise.all([
+      Artist.remove({}),
+      User.remove({})
+    ])
+      .then(() => Artist.create(artistData))
+      .then(() => User.create(userData))
+      .then(user => {
+        token = jwt.sign({ sub: user._id }, secret, { expiresIn: '6h' });
+      })
+      .then(done);
+  });
+
+
+  it('should return a 401 response without token', done => {
+    api
+      .post('/api/artists')
+      .send(artistData)
+      .end((err, res) => {
+        expect(res.status).to.eq(401);
+        done();
+      });
+  });
+
+  it('should return a 201 response with a token', done => {
+    api
+      .post('/api/artists')
+      .set('Authorization', `Bearer ${token}`)
+      .send(artistData)
+      .end((err, res) => {
+        expect(res.status).to.eq(201);
+        done();
+      });
+  });
+
+  it('should return the created artist', done => {
+    api
+      .post('/api/artists')
+      .set('Authorization', `Bearer ${token}`)
+      .send(artistData)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.include.keys([
+          '_id',
+          'name',
+          'image',
+          'dateBorn',
+          'dateDeath',
+          'info',
+          'wikiLink'
+        ]);
+        done();
+      });
+  });
+
+  it('should return the correct data', done => {
+    api
+      .post('/api/artists')
+      .set('Authorization', `Bearer ${token}`)
+      .send(artistData)
+      .end((err, res) => {
+        expect(res.body.name).to.eq(artistData.name);
+        expect(res.body.image).to.eq(artistData.image);
+        expect(res.body.dateBorn).to.eq(artistData.dateBorn);
+        expect(res.body.dateDeath).to.eq(artistData.dateDeath);
+        expect(res.body.info).to.eq(artistData.info);
+        expect(res.body.wikiLink).to.eq(artistData.wikiLink);
+
+        done();
+      });
+  });
+
+});
