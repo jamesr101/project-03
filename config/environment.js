7a35025220f2cb70700b8bfe57c09b7d304197f5
James Robins <jamesrobins101@hotmail.com>
2018-10-10 14:17:03 +0100

    set up for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev';
-const dbURI = `mongodb://localhost:27017/artmapper-${env}`;
-const secret = 'Hjs*sa/%sj-pZas';
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost:27017/artmapper-${env}`;
+const secret = process.env.SECRET || 'Hjs*sa/%sj-pZas';
 
 // https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Pablo_Picasso
 //
